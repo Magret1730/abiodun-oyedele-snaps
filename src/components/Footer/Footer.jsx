0f5d2880e81f86f48aa5ce2077df5f750aecaeb8
Magret <belloabiodun17@gmail.com>
2025-02-12 17:12:20 -0330

    Implemented the tablet breakpoint on the page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,32 +4,45 @@ import NavLogo from "../NavLogo/NavLogo";
 export default function Footer() {
     return (
         <article className="footer">
-            <section>
-                <NavLogo />
-            </section>
             <section className="footer__container">
-                <div className="footer__contents">
-                    <p>For photographers</p>
-                    <p>Hire talents</p>
-                    <p>Inspiration</p>
-                </div>
-                <div className="footer__content">
-                    <p>About</p>
-                    <p>Careers</p>
-                    <p>Support</p>
-                </div>
-                <div className="footer__socials">
+                <section className="footer__logo">
+                    <NavLogo />
+                </section>
+
+                <section className="footer__box">
+                    <div className="footer__contents">
+                        <p>For photographers</p>
+                        <p>Hire talents</p>
+                        <p>Inspiration</p>
+                    </div>
+                    <div className="footer__content">
+                        <p>About</p>
+                        <p>Careers</p>
+                        <p>Support</p>
+                    </div>
+                    <div className="footer__socials">
+                        <img src="../../assets/Icons/Facebook.svg" alt="Facebook" />
+                        <img src="../../assets/Icons/X_twitter.svg" alt="Twitter" />
+                        <img src="../../assets/Icons/Facebook.svg" alt="Instagram" />
+                        <img src="../../assets/Icons/Pinterest.svg" alt="Pinterest" />
+                    </div>
+                </section>
+            </section>
+
+            <section className="footer__securities">
+                <div className="footer__securities-socials">
                     <img src="../../assets/Icons/Facebook.svg" alt="Facebook" />
                     <img src="../../assets/Icons/X_twitter.svg" alt="Twitter" />
                     <img src="../../assets/Icons/Facebook.svg" alt="Instagram" />
                     <img src="../../assets/Icons/Pinterest.svg" alt="Pinterest" />
                 </div>
-            </section>
-            <section className="footer__securities">
-                <div>&copy;2024 Snaps</div>
-                <div>Terms</div>
-                <div>Privacy</div>
-                <div>Cookies</div>
+
+                <div className="footer__securities-terms">
+                    <div>&copy;2024 Snaps</div>
+                    <div>Terms</div>
+                    <div>Privacy</div>
+                    <div>Cookies</div>
+                </div>
             </section>
         </article>
     )
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
     return (
         <section className="hero">
             <h4 className="hero__header">Our mission:</h4>
-            <div className="hero__texts">Provides photographers a space to share photos of the neighbourhoods they cherish,
+            <div className="hero__texts">Provide photographers a space to share photos of the neighbourhoods they cherish,
                 <span className="hero__texts-span"> expressed in their unique style.</span></div>
         </section>
     )
