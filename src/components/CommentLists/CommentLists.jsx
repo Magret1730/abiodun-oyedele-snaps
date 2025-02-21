7fd5cf34a074ceb8b5323066fd553671c897ec82
Magret <belloabiodun17@gmail.com>
2025-02-20 21:03:38 -0330

    Add tablet and desktop responsiveness on comment section in photo page

--- src/components/CommentLists/CommentLists.jsx
+++ src/components/CommentLists/CommentLists.jsx
@@ -4,13 +4,13 @@ import Comment from "../Comment/Comment";
 export default function CommentLists({ comments }) {
 
     return (
-        <>
-            <section className="comment__length">{comments.length} Comments</section>
+        <section className="comments">
+            <section className="comments__length">{comments.length} Comments</section>
             {comments
             .sort((a, b) => b.timestamp - a.timestamp)
             .map((comment) => {
                 return <Comment comment={comment} key={comment.id}/>
             })}
-        </>
+        </section>
     )
 }
