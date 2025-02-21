c410ebe2fbcebd1cc141129cf5a53b10e870605e
Magret <belloabiodun17@gmail.com>
2025-02-20 20:40:41 -0330

    Fetch tags based on API rather than json file

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,6 @@ import Nav from "../../components/Nav/Nav";
 import Hero from "../../components/Hero/Hero";
 import CardList from "../../components/CardList/CardList";
 import Footer from "../../components/Footer/Footer";
-import TagList from "../../data/tags.json";
 import Tags from "../../components/Tags/Tags";
 import axios from "axios";
 
@@ -13,12 +12,17 @@ function Home() {
     const [isActive, setIsActive] = useState(false);
     const [activeTags, setActiveTags] = useState(null);
     const [filteredPhotos, setFilteredPhotos] = useState([]);
+    const [tags, setTags] = useState(null);
 
     const BASE_URL = "https://unit-3-project-c5faaab51857.herokuapp.com";
     const API_KEY = "e3b638d4-7a00-4b19-8713-677d535d16cc";
 
     useEffect(() => {
         fetchPhotos();
+    }, []);
+
+    useEffect(() => {
+        fetchTags();
     }, []);
 
     async function fetchPhotos() {
@@ -32,6 +36,20 @@ function Home() {
     }
 
     if (!photos) {
+        return <div>loading...</div>
+    }
+
+    async function fetchTags() {
+        try {
+            const {data} = await axios.get(`${BASE_URL}/tags?api_key=${API_KEY}`);
+            setTags(data);
+
+        } catch (error) {
+            console.error("Error fetching tags", error);
+        }
+    }
+
+    if (!tags) {
         return <div>loading...</div>
     }
 
@@ -58,22 +76,22 @@ function Home() {
 
     return (
         <main className={ `${isActive ? "" : "home"}` }>
-        <Nav clickHandler={onClick} isActive={isActive}/>
+            <Nav clickHandler={onClick} isActive={isActive}/>
 
-        <section className={ `${isActive ? "home__container" : ""}` }>
-            <aside className={ `${isActive ? "home__aside" : "" }` }>
-            { isActive && 
-                <Tags tags={TagList} className={"tags__filter"} handleTagsClick={handleFilterClick} activeTags={activeTags} />
-            }
-            </aside>
+            <section className={ `${isActive ? "home__container" : ""}` }>
+                <aside className={ `${isActive ? "home__aside" : "" }` }>
+                    { isActive && 
+                        <Tags tags={tags} className={"tags__filter"} handleTagsClick={handleFilterClick} activeTags={activeTags} />
+                    }
+                </aside>
 
-            <section>
-            <Hero isActive={isActive} />
-            <CardList isActive={isActive} photos={filteredPhotos} />
+                <section>
+                    <Hero isActive={isActive} />
+                    <CardList isActive={isActive} photos={filteredPhotos} />
+                </section>
             </section>
-        </section>
 
-        <Footer />
+            <Footer />
         </main>
     )
 }
